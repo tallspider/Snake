9d006283da0b435a4554f3fbebfa0f0ac3e8f835
tallspider <qijia.gao@mail.utoronto.ca>
2020-04-05 19:34:32 -0400

    polled I/O for direction change

--- main.c
+++ main.c
@@ -15,13 +15,17 @@ struct snake_list{
 
 //file globals**********************************************************************************
 volatile int pixel_buffer_start; // global variable
+volatile int * pixel_ctrl_ptr;
+volatile int * edge_ptr;
 
 struct snake_list snake;
-int apple_rb, apple_cb, snake_dx, snake_dy, snake_x, snake_y, snake_length;
+struct snake_node *prev_tail; 		//need this to separate drawing from updating snake location
+
+int apple_rb, apple_cb, snake_drb, snake_dcb, snake_x, snake_y, snake_length;
 short int apple_colour = 0xF800; // red
 short int snake_colour = 0x03E0; // dark green
 int score = 0;
-bool legal_move = false, run = false, eat = false, back_apple_drawn;
+bool legal_move = false, run = false, eat = false;
 bool game_started;
 
 //prototypes*************************************************************************************
@@ -34,21 +38,28 @@ void draw_box(int x, int y, short int colour);
 void draw_block(int rb, int cb, short int color);
 void swap(int * one, int * two);
 
+void check_direction_change();
+
 void init_snake();
 void draw_first_snake();
+void move_snake();
+void update_snake_drawing();
+void lengthen_snake();
+void draw_longer_snake();
 void free_snake();
 
 void init_apple();
 void draw_apple();
 
 bool is_legal(int x, int y);
-bool has_eaten(int x0, int y0, int x1, int y1);
+bool check_move_legal();
+bool will_eat_apple();
 
 //implementations**********************************************************************************
 int main(void) {
-    volatile int * pixel_ctrl_ptr = (int *)0xFF203020;
-    volatile int * edge_ptr = (int *)0xFF20005C; // edgecapture register
-
+    pixel_ctrl_ptr = (int *)0xFF203020;
+    edge_ptr = (int *)0xFF20005C; // edgecapture register
+	
 	//background setup ----------------------------------------------------------------------------
     /* set front pixel buffer to start of FPGA On-chip memory */
     *(pixel_ctrl_ptr + 1) = 0xC8000000; // first store the address in the back buffer
@@ -62,13 +73,13 @@ int main(void) {
     clear_screen(); 
 	draw_borders();
 	
-    /* set back pixel buffer to start of SDRAM memory */
-    *(pixel_ctrl_ptr + 1) = 0xC0000000;
+    // /* set back pixel buffer to start of SDRAM memory */
+    // *(pixel_ctrl_ptr + 1) = 0xC0000000;
 	
-    pixel_buffer_start = *(pixel_ctrl_ptr + 1); // we draw on the back buffer
+	// pixel_buffer_start = *(pixel_ctrl_ptr + 1); // we draw on the back buffer
 
-    clear_screen();
-	draw_borders(); // draw game borders
+    // clear_screen();
+	// draw_borders(); // draw game borders
     
 	//game setup-----------------------------------------------------------------------------------
     
@@ -86,41 +97,39 @@ int main(void) {
 	draw_first_snake();
 	draw_apple();
 	
-	wait_for_vsync();
-	pixel_buffer_start = *(pixel_ctrl_ptr + 1);
+	// wait_for_vsync();
+	// pixel_buffer_start = *(pixel_ctrl_ptr + 1);
 	
-	draw_first_snake();
-	draw_apple();
-	
-	back_apple_drawn = true;
+	// draw_first_snake();
+	// draw_apple();
 	
 	while (run) {
-    	// erase prev snake tail
+		// erase prev snake tail
     	// polled IO for KEYs to determine which direction the snake moves in
     	// check key value and change dy or dx as necessary
     	// KEY0 = right, KEY1 = left, KEY2 = down, KEY3 = up
 		// insert function that changes direction snake moves in correctly
 		// increment dy or dx
-		//draw_box(snake_x, snake_y, snake_colour);
+		// draw_box(snake_x, snake_y, snake_colour);
 		
-		if(!back_apple_drawn){	//new apple has not been drawn on this canvas yet
-			draw_apple();
-			back_apple_drawn = true;
-		}
-		
-		//move snake to next position
-		
-		// snake_x += snake_dx;
-		// snake_y += snake_dy;
+		// check edge capture first 
+		check_direction_change();
 		
 		// check if legal
-		if (legal_move) {
+		if (check_move_legal()) {
 			
-			//legal_move = false;
-			//eat = has_eaten(snake_x, snake_y, apple_x, apple_y);
-			
-			if (eat) {
-				eat = false;
+			if (will_eat_apple()) {
+				
+				//apple is now part of the snake
+				lengthen_snake();
+				
+				//draw changes to the snake
+				draw_longer_snake();
+				
+				// wait_for_vsync();
+				// pixel_buffer_start = *(pixel_ctrl_ptr + 1); // new back buffer
+				
+				// draw_longer_snake();
 				
 				// re-initialization of apple in a random position if snake has 'eaten' apple
 				do{
@@ -128,21 +137,35 @@ int main(void) {
 					apple_rb = (rand() % 44) + 2; // range from 2 to 45
 				} while (!is_legal(apple_rb, apple_cb));	//as long as apple is not on snake
 				
+				//draw_changes to the apple
 				draw_apple();
-				back_apple_drawn = false; //need to draw on back too
 				
-				// update snake length
-				//snake_length++;
+				//draw on back canvas too
+				// wait_for_vsync();
+				// pixel_buffer_start = *(pixel_ctrl_ptr + 1); // new back buffer
+				
+				// draw_apple();
 				
 				// update score
 				score++;
 				
+			} else {
+				
+				move_snake();
+				
+				//draw on this canvas
+				update_snake_drawing();
+				
+				//draw on back canvas too
+				// wait_for_vsync();
+				// pixel_buffer_start = *(pixel_ctrl_ptr + 1); // new back buffer
+				
+				// update_snake_drawing();
 			}
 			
 		} else {    
 			// else game over
 			// game over screen
-			wait_for_vsync();
 			run = false;
 			break;
 		}
@@ -158,8 +181,8 @@ int main(void) {
 void init_snake(){
 	// snake_x = 159;
     // snake_y = 119;
-    snake_dx = 0;
-    snake_dy = 0;
+    snake_drb = 0;
+    snake_dcb = 1;
 	// snake_length = 1;
 	
 	snake.head = (struct snake_node *)malloc(sizeof(struct snake_node));
@@ -167,6 +190,8 @@ void init_snake(){
 	snake.head -> rb = 24;
 	snake.head -> cb = 32;
 	snake.head -> prev = NULL;
+	
+	prev_tail = NULL;
 }
 
 void init_apple(){
@@ -180,6 +205,43 @@ void draw_first_snake(){
 
 void draw_apple(){
 	draw_block(apple_rb, apple_cb, apple_colour);
+}
+
+void move_snake(){
+	//make new snake node for head, take off tail
+	struct snake_node *new_head = (struct snake_node *)malloc(sizeof(struct snake_node));
+	new_head -> rb = snake.head -> rb + snake_drb;
+	new_head -> cb = snake.head -> cb + snake_dcb;
+	new_head -> prev = NULL;
+	
+	snake.head -> prev = new_head;
+	snake.head = snake.head -> prev;
+	
+	//take off the old tail
+	if(prev_tail != NULL) free(prev_tail);
+	prev_tail = snake.tail;
+	snake.tail = snake.tail -> prev;
+}
+
+void lengthen_snake(){
+	struct snake_node *new_head = (struct snake_node *)malloc(sizeof(struct snake_node));
+	new_head -> rb = apple_rb;
+	new_head -> cb = apple_cb;
+	new_head -> prev = NULL;
+	
+	snake.head -> prev = new_head;
+	snake.head = snake.head -> prev;
+}
+
+void draw_longer_snake(){
+	draw_block(apple_rb, apple_cb, snake_colour);
+}
+
+void update_snake_drawing(){
+	if(prev_tail != NULL)
+		draw_block(prev_tail -> rb, prev_tail -> cb, 0);
+	
+	draw_block(snake.head -> rb, snake.head -> cb, snake_colour);
 }
 
 void free_snake(){
@@ -303,6 +365,57 @@ void draw_borders() {
 		
 }
 
+void check_direction_change(){
+	if((*edge_ptr) & 0xF){
+			
+		if((*edge_ptr) & 0x8){		//up
+			
+			if(snake_drb != 1){		//cannot reverse
+				snake_drb = -1;
+				snake_dcb = 0;
+			}
+		} else if((*edge_ptr) & 0x4){	//down
+			
+			if(snake_drb != -1){
+				snake_drb = 1;
+				snake_dcb = 0;
+			}
+		} else if((*edge_ptr) & 0x2){	//left 
+			
+			if(snake_dcb != 1){
+				snake_drb = 0;
+				snake_dcb = -1;
+			}
+		} else {
+			
+			if(snake_dcb != -1){
+				snake_drb = 0;
+				snake_dcb = 1;
+			}
+		}
+		
+		//reset edge capture
+		*edge_ptr = 0xF;
+	}
+}
+
+bool check_move_legal(){
+	int next_rb = snake.head -> rb + snake_drb;
+	int next_cb = snake.head -> cb + snake_dcb;
+	
+	//first check if they are both in range
+	if(next_rb < 2 || next_rb > 45 || next_cb < 2 || next_cb > 61) return false;
+	
+	//next check if snake will eat itself
+	struct snake_node *temp = snake.tail;
+	while(temp != NULL){
+		if(next_rb == temp -> rb && next_cb == temp -> cb) return false;
+		temp = temp -> prev;
+	}
+	
+	return true;
+}
+
 bool is_legal(int apple_rb, int apple_cb) {
 	//check if the apple is on the snake
 	struct snake_node *temp = snake.tail;
@@ -313,8 +426,10 @@ bool is_legal(int apple_rb, int apple_cb) {
 	return true;
 }
 
-bool has_eaten(int x0, int y0, int x1, int y1) {
-	return ((x0 == x1) && (y0 == y1));
+bool will_eat_apple() {
+	int next_rb = snake.head -> rb + snake_drb;
+	int next_cb = snake.head -> cb + snake_dcb;
+	return next_rb == apple_rb && next_cb == apple_cb;
 }
 
 /*
